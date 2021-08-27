6e0f720dba37d0d44bede98225ae7e2db259a257
BuZzbkm <bekamadawit@gmail.com>
2021-08-27 22:31:32 +0300

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,8 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
-	{
+	for (n = 0; n < 10; n++)
 		putchar(n);
-		if (n != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
+	        putchar(", ");
 	return (0);
+}
